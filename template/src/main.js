092b2ef14447789f240e2fd3124b7f25b3ecfecf
venus <jinxing@birdpush.com>
2018-02-26 15:46:05 +0800

    加入模板

--- template/src/main.js
+++ template/src/main.js
@@ -11,7 +11,6 @@ import router from './router'
 import store from './store'
 {{/vuex}}
 
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
---
+++ template/src/views/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page page-index">
+    默认路由页面  Index   
+  </div>
+</template>
+<script>
+export default {
+  name: "Index",
+  data () {
+    return {
+            
+    };
+  },
+  computed:{
+            
+  },
+  mounted(){
+            
+  },
+  methods:{
+            
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import './../styles/common';
+
+.page-index {
+  color: $color;
+}
+</style>
